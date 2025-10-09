052da9bc30324c59132aceb2d1b2758d0aa2d6de
Mikhaeli <Mihaelirekhviashvili@gmail.com>
2025-10-09 18:52:40 +0400

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Credo on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
