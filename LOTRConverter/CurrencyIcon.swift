5f9f6d7a63e112a297747e25a9ba8ff30dd2368b
Mikhaeli <Mihaelirekhviashvili@gmail.com>
2025-10-26 15:59:58 +0400

    currency selectable component

---
+++ LOTRConverter/CurrencyIcon.swift
@@ -0,0 +1,37 @@
+//
+//  ExchangeRate.swift
+//  LOTRConverter
+//
+//  Created by Credo on 10/25/25.
+//
+
+import SwiftUI
+
+struct CurrencyIcon: View {
+    let currencyImage: ImageResource
+    let currencyName: String
+    
+    
+    var body: some View {
+        ZStack(alignment: .bottom){
+            Image(currencyImage)
+                .resizable()
+                .scaledToFit()
+            
+            Text(currencyName)
+                .padding(3)
+                .font(.caption)
+                .frame(maxWidth: .infinity)
+                .background(.brown.opacity(0.75))
+        }
+        .padding(3)
+        .frame(width: 100, height: 100)
+        .background(.brown)
+        .clipShape(.rect(cornerRadius: 25))
+        
+    }
+}
+
+#Preview {
+    CurrencyIcon(currencyImage: .goldpiece, currencyName: "Gold Piece")
+}
